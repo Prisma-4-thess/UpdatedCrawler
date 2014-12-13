ff3f70b3587bbe8f16aeda9fe5acdc3fe8585a6e
PuR3v1L <flojim4@gmail.com>
2014-12-13 17:36:45 +0200

    Organizations
    Code to fetch and print all available organizations

--- mainCrawler.py
+++ mainCrawler.py
@@ -9,21 +9,21 @@ import sys
 query_size = 500;
 page = 0;
 
-def findAllOrgForPerifereia (response):
-	'''Find all organizations that have as supervisor Perifereia Kentrikis Makedonias
+# def findAllOrgForPerifereia (response):
+# 	'''Find all organizations that have as supervisor Perifereia Kentrikis Makedonias
 
-	Arguments:
-	response: a json response returned from OpendataClient 
-	'''
-	file = open("organizationsPeriferia", "w")
-	organizations = response["organizations"]
-	file.write("22898"+"\n")
-	file.write("50205"+"\n")
-	for org in organizations:
-		if (org["supervisorId"]=="5009"):
-			file.write(org["uid"]+"\n")
-			print org["uid"]+":"+org["latinName"]
-	file.close()
+# 	Arguments:
+# 	response: a json response returned from OpendataClient 
+# 	'''
+# 	file = open("organizationsPeriferia", "w")
+# 	organizations = response["organizations"]
+# 	file.write("22898"+"\n")
+# 	file.write("50205"+"\n")
+# 	for org in organizations:
+# 		if (org["supervisorId"]=="5009"):
+# 			file.write(org["uid"]+"\n")
+# 			print org["uid"]+":"+org["latinName"]
+# 	file.close()
 
 def printAllTypes (response):
 	'''Print all the types' labels
@@ -47,20 +47,20 @@ def printAllDictionaries (response):
 		print i["label"]
 
 
-def printAllDecisionsPDF (response,filename):
-	'''Print the urls of all decisions
+# def printAllDecisionsPDF (response,filename):
+# 	'''Print the urls of all decisions
 
-	Arguments:
-	response: a json response returned from OpendataClient 
-	'''
-	file = open(filename, "a")
-	decisions = response["decisions"]
-	for decision in decisions:
-		for i in decision:
-			if (i==u'url'):
-				# print i,":",decision[i]
-				file.write(decision[i].encode('utf-8')+"/document"+"\n")
-	file.close()
+# 	Arguments:
+# 	response: a json response returned from OpendataClient 
+# 	'''
+# 	file = open(filename, "a")
+# 	decisions = response["decisions"]
+# 	for decision in decisions:
+# 		for i in decision:
+# 			if (i==u'url'):
+# 				# print i,":",decision[i]
+# 				file.write(decision[i].encode('utf-8')+"/document"+"\n")
+# 	file.close()
 
 def printInfo (response):
 	'''Return the total size of decisions
@@ -74,6 +74,32 @@ def printInfo (response):
 		if (i==u'total'):
 			return info[i]
 
+def printOrganizations (response):
+	'''Return all organizations from diavgeia
+
+	Arguments
+	response: a json response returned from OpendataClient
+	'''
+	organizations = response["organizations"]
+	for organization in organizations:
+		print "Organization "+organization["uid"]
+		for key in organization:
+			# print organization[key]!=None
+			# print key
+			if (organization[key]!=None and key!='uid' and key!='organizationDomains'):
+				print "\t"+key+": "+organization[key]
+
+def printOrganizations (response):
+	organizations = response["organizations"]
+	for organization in organizations:
+		print "Organization "+organization["uid"]
+		for key in organization:
+			# print organization[key]!=None
+			# print key
+			if (organization[key]!=None and key!='uid' and key!='organizationDomains'):
+				print "\t"+key+": "+organization[key]
+		
+
 def main(argv=None):
 	client = opendata.OpendataClient("https://diavgeia.gov.gr/luminapi/opendata")
 	# print "***TYPES***"
@@ -82,24 +108,25 @@ def main(argv=None):
 	# print "***DICTIONARIES***"
 	# response = client.get_dictionaries()
 	response = client.get_organizations()
+	printOrganizations(response)
 	# print (response);
-	findAllOrgForPerifereia(response)
+	
 	# printAllDictionaries(response)
-	content = [line.strip() for line in open('organizationsPeriferia')]
-	for organization in content:
-		q = "submissionTimestamp:[DT(2006-03-01T00:00:00) TO DT(2014-11-11T23:59:59)] AND (organizationUid:"+organization+")"
-		print q;
-		response = client.get_advanced_search_results(q,page,query_size)
-		total = printInfo (response)
-		# print (total)
-		steps = total/query_size
-		# print (steps)
-		# print ("***ACTUAL DECISIONS***")
-		printAllDecisionsPDF(response,organization)
-		for x in range(1,steps+1):
-			print("Page ",x)
-			response = client.get_advanced_search_results(q,x,query_size)
-			printAllDecisionsPDF(response,organization)
+	# content = [line.strip() for line in open('organizationsPeriferia')]
+	# for organization in content:
+	# 	q = "submissionTimestamp:[DT(2006-03-01T00:00:00) TO DT(2014-11-11T23:59:59)] AND (organizationUid:"+organization+")"
+	# 	print q;
+	# 	response = client.get_advanced_search_results(q,page,query_size)
+	# 	total = printInfo (response)
+	# 	# print (total)
+	# 	steps = total/query_size
+	# 	# print (steps)
+	# 	# print ("***ACTUAL DECISIONS***")
+	# 	printAllDecisionsPDF(response,organization)
+	# 	for x in range(1,steps+1):
+	# 		print("Page ",x)
+	# 		response = client.get_advanced_search_results(q,x,query_size)
+	# 		printAllDecisionsPDF(response,organization)
 	return 0
 
 if __name__ == "__main__":
